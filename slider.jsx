8bf6b80fc695e5c2309bfef3942feb1e38334e65
carlosJiachen <65225817@qq.com>
2020-04-12 09:58:54 +0800

    create

--- slider.jsx
+++ slider.jsx
@@ -1,9 +1,9 @@
-# tools
-# /*
+/*
  * @Author: liujiachen
  * @Date: 2020-04-12 09:38:18
  * @LastEditTime: 2020-04-12 09:51:13
  * @LastEditors: Please set LastEditors
  * @Description:  This project is used to save my js tools based of react
  * @FilePath: \tools\slider.jsx
-# */
+ */
+
