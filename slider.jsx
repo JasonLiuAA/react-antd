bc8c063df6fba04a65b5b5b44ca6541f94092baf
carlosJiachen <65225817@qq.com>
2020-04-12 10:22:29 +0800

    test

--- slider.jsx
+++ slider.jsx
@@ -1,9 +1,8 @@
 /*
  * @Author: liujiachen
  * @Date: 2020-04-12 09:38:18
- * @LastEditTime: 2020-04-12 09:51:13
+ * @LastEditTime: 2020-04-12 10:21:10
  * @LastEditors: Please set LastEditors
  * @Description:  This project is used to save my js tools based of react
  * @FilePath: \tools\slider.jsx
  */
-
